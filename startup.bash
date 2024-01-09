a31aaa2a16b05d13720fa6eca3b64a8456a2c9cc
Kanto <pyae.phyo.khant@rutilea.com>
2024-01-09 14:55:48 +0900

    startup script for server

---
+++ startup.bash
@@ -0,0 +1,26 @@
+sudo apt update -y
+sudo apt upgrade -y
+
+# Docker install
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# boot up project
+git clone https://github.com/PyaePhyoKanto/DummyDjangoReactML.git
+cd DummyDjangoReactML
+gsutil cp gs://env05jan24/.env .env
+sudo docker compose up -d --build
+sudo docker exec -it dummydjangoreactml-web-1 python manage.py migrate
