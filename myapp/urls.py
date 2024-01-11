918466caa4e86caae35005d8e1301dd192f47905
Kanto <pyae.phyo.khant@rutilea.com>
2024-01-11 17:53:03 +0900

    training API in Django

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet
+from .views import ItemViewSet, start_ml_training
 
 router = DefaultRouter()
 router.register(r'items', ItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('start-ml-training/', start_ml_training, name='start-ml-training'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,10 @@ from rest_framework import viewsets
 from .models import Item
 from .serializers import ItemSerializer
 from django.http import HttpResponse
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+import requests
+
 
 def home_view(request):
     return HttpResponse("Welcome to My Django App!")
@@ -9,3 +13,8 @@ def home_view(request):
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+@api_view(['GET', 'POST'])
+def start_ml_training(request):
+    response = requests.post('http://ml:5000/start-training')
+    return JsonResponse(response.json())
